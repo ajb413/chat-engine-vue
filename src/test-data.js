fea2cf5c58da101a16610d3e103778aa9edeb2a3
Adam Bavosa <adamb@pubnub.com>
2018-02-24 16:00:34 -0800

    moved test data to file

---
+++ src/test-data.js
@@ -0,0 +1,76 @@
+export default {
+  friends: [
+    {
+      avatar: 'https://lh3.googleusercontent.com/-nr6r2jf30wY/AAAAAAAAAAI/AAAAAAAAAio/mthZ3J53MWI/s120-p-rw-no/photo.jpg',
+      name: 'Adam Bavosa',
+      lastMessage: "No I can't come to your party tomorrow. I really don't like you so why should I be near you ever?"
+    },
+    {
+      avatar: 'https://lh3.googleusercontent.com/-bDhMIBLV0P0/AAAAAAAAAAI/AAAAAAAADUM/8KISzLgIh4s/s60-p-rw-no/photo.jpg',
+      name: 'Stephen Blum',
+      lastMessage: 'deluxe!'
+    }
+  ],
+  messages: [
+    {
+      who: 'them',
+      time: 1519420960740,
+      text: 'hey'
+    },
+    {
+      who: 'them',
+      time: 1519420960741,
+      text: 'hey'
+    },
+    {
+      who: 'them',
+      time: 1519420960742,
+      text: 'hey'
+    },
+    {
+      who: 'them',
+      time: 1519420960743,
+      text: 'hey'
+    },
+    {
+      who: 'them',
+      time: 1519420960751,
+      text: 'hey'
+    },
+    {
+      who: 'them',
+      time: 1519420960752,
+      text: 'hey'
+    },
+    {
+      who: 'them',
+      time: 1519420960753,
+      text: 'hey'
+    },
+    {
+      who: 'them',
+      time: 1519420960754,
+      text: 'hey'
+    },
+    {
+      who: 'them',
+      time: 1519420960763,
+      text: 'hey wanna come to my party?'
+    },
+    {
+      who: 'them',
+      time: 1519420960765,
+      text: 'thursday at 8'
+    },
+    {
+      who: 'me',
+      time: 1519420960764,
+      text: 'when is it'
+    },
+    {
+      who: 'me',
+      time: 1519420960766,
+      text: "No I can't come to your party tomorrow. I really don't like you so why should I be near you ever?"
+    }
+  ]
+}
