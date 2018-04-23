c8534602e71b006a2e039abb78fb914ae523bd63
Adam Bavosa <adamb@pubnub.com>
2018-04-22 20:24:13 -0700

    added chatbot and refactor

--- src/main.js
+++ src/main.js
@@ -5,12 +5,20 @@ import App from './App';
 import store from './store';
 import VueChatEngine from 'vue-chat-engine';
 import ChatEngineCore from 'chat-engine';
+import typingIndicator from './typing-indicator';
 import DefaultChat from './default-chats';
+import botInit from './bot';
+import util from './util';
+
+Vue.config.productionTip = false;
 
 // Sets the default global and 1:1 chats
 store.commit('setFriends', {
   friends: DefaultChat.friends,
 });
+
+// ChatBot REST endpoint powered by PubNub Functions and Amazon Lex
+const chatBotURL = 'https://pubsub.pubnub.com/v1/blocks/sub-key/sub-c-eb9ac3fa-248e-11e8-bb29-5a43d096f02f/chat-bot';
 
 // Init ChatEngine with PubNub
 const pub = 'pub-c-bc85cb90-3bb6-4b48-9f2e-73e7a60a7be5';
@@ -25,36 +33,88 @@ const chatEngine = ChatEngineCore.create({
   subscribeKey: sub,
 }, {
   globalChannel: store.state.friends[0].chatKey,
-  enableSync: true,
 });
 
-Vue.config.productionTip = false;
-
-/**
- * Get a new UUID
- * @return {string} A 4 character UUID for PubNub and to give to friends.
- */
-function fourCharUUID() {
-  const maxLength = 4;
-  const possible = 'abcdef0123456789';
-  let text = '';
-
-  for (let i = 0; i < maxLength; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-
-  return text;
-}
-
-const myUuid = fourCharUUID();
-
+const myUuid = util.fourCharUUID();
 const me = {
   name: myUuid,
   uuid: myUuid,
 };
 
-// Chat Engine injected into every component instance
+// ChatEngine injected into every component instance
 Vue.use(VueChatEngine, {chatEngine, store});
+
+/**
+ * Execute this function when the Vue instance is created.
+ */
+function created() {
+  const ChatEngine = this.$chatEngine;
+  const store = this.$store;
+
+  ChatEngine.connect(me.uuid, me);
+
+  document.addEventListener('beforeunload', function() {
+    ChatEngine.disconnect();
+  });
+
+  ChatEngine.on('$.ready', function(data) {
+    // store my new user as `me`
+    let me = data.me;
+    store.commit('setMe', {me});
+
+    ChatEngine.global.search({
+      event: 'message',
+      limit: 6,
+    });
+
+    ChatEngine.global.key = store.state.friends[0].chatKey;
+
+    store.commit('setCurrentChat', {
+      chatKey: ChatEngine.global.key,
+    });
+
+    // Create a new chat for each user in the friends list
+    store.state.friends.forEach(function(friend, index) {
+      // Make a private chat key with the Stephen bot
+      if (!friend.chatKey) {
+        let uuids = [friend.uuid, store.state.me.uuid].sort();
+        friend.chatKey = uuids.join('-');
+
+        store.commit('updateFriendChatKey', {
+          friendIndex: index,
+          chatKey: friend.chatKey,
+        });
+
+        // Init ChatBot with its own ChatEngine instance (bot.js)
+        botInit(ChatEngine, friend, chatBotURL);
+      }
+
+      if (!store.state.chats[friend.chatKey]) {
+        // Create a new ChatEngine Chat, true for private chat
+        let myChat = new ChatEngine.Chat(friend.chatKey, true);
+
+        // Add the Typing Indicator ChatEngine plugin to this 1:1 chat.
+        typingIndicator(myChat);
+
+        // when a user comes online
+        myChat.on('$.online.*', (data) => {
+          console.log('New user', data.user.uuid);
+        });
+
+        // when a user goes offline
+        myChat.on('$.offline.*', (data) => {
+          console.log('User left', data.user.uuid);
+        });
+
+        myChat.key = friend.chatKey;
+
+        store.commit('newChat', {
+          chat: myChat,
+        });
+      }
+    });
+  });
+}
 
 /* eslint-disable no-new */
 new Vue({
@@ -62,62 +122,5 @@ new Vue({
   store,
   components: {App},
   template: '<App/>',
-  created() {
-    const ChatEngine = this.$chatEngine;
-    const store = this.$store;
-
-    ChatEngine.connect(me.uuid, me);
-
-
-    ChatEngine.on('$.ready', function(data) {
-      // store my new user as `me`
-      let me = data.me;
-      store.commit('setMe', {me});
-
-      ChatEngine.global.search({
-        event: 'message',
-        limit: 6,
-      });
-
-      ChatEngine.global.key = store.state.friends[0].chatKey;
-
-      store.commit('setCurrentChat', {
-        chatKey: ChatEngine.global.key,
-      });
-
-      // Create a new chat for each user in the friends list
-      store.state.friends.forEach(function(friend, index) {
-        // Make a private chat key with the Stephen bot
-        if (!friend.chatKey) {
-          let uuids = [friend.uuid, store.state.me.uuid].sort();
-          friend.chatKey = uuids.join('-');
-          store.commit('updateFriendChatKey', {
-            friendIndex: index,
-            chatKey: friend.chatKey,
-          });
-        }
-
-        if (!store.state.chats[friend.chatKey]) {
-          // create a new ChatEngine Chat
-          let myChat = new ChatEngine.Chat(friend.chatKey);
-
-          // when a user comes online, render them in the online list
-          myChat.on('$.online.*', (data) => {
-            // $('#people-list ul').append(peopleTemplate(data.user));
-          });
-
-          // when a user goes offline, remove them from the online list
-          myChat.on('$.offline.*', (data) => {
-            // $('#people-list ul').find('#' + data.user.uuid).remove();
-          });
-
-          myChat.key = friend.chatKey;
-
-          store.commit('newChat', {
-            chat: myChat,
-          });
-        }
-      });
-    });
-  },
+  created,
 });
